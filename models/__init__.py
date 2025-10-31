85bdd9c2fbae9638a7db128de53b0a672a5cb8d5
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-11-01 00:07:22 +0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
-from . import monta_sale_log
+# from . import monta_sale_log
 from . import product_product
 from . import product_template
 from . import sale_order
 from . import sale_order_line
-from . import sku_test_log
+# from . import sku_test_log
 from . import sale_order_inbound
 # from . import inbound_expected_delivery
 from . import monta_order_status
